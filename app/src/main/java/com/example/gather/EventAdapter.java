c207bbaa30e363a5234d7b657b13b099f714e386
Drew <drewpap@umich.edu>
2020-02-22 22:02:01 -0500

    Added fragment support for creating events.

--- app/src/main/java/com/example/gather/EventAdapter.java
+++ app/src/main/java/com/example/gather/EventAdapter.java
@@ -1,57 +1,57 @@
-package com.example.gather;
-
-import android.view.LayoutInflater;
-import android.view.ViewGroup;
-import android.widget.TextView;
-import com.example.gather.Event;
-
-import androidx.recyclerview.widget.RecyclerView;
-
-public class EventAdapter extends RecyclerView.Adapter<EventAdapter.MyViewHolder> {
-    private Event[] mDataset;
-
-// Provide a reference to the views for each data item
-// Complex data items may need more than one view per item, and
-// you provide access to all the views for a data item in a view holder
-    public static class MyViewHolder extends RecyclerView.ViewHolder {
-        // each data item is just a string in this case
-        public TextView textView;
-        public MyViewHolder(TextView v) {
-            super(v);
-            textView = v;
-        }
-    }
-
-    // Provide a suitable constructor (depends on the kind of dataset)
-    public EventAdapter(Event[] myDataset) {
-        mDataset = myDataset;
-    }
-
-    // Create new views (invoked by the layout manager)
-    @Override
-    public EventAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent,
-                                                        int viewType) {
-        // create a new view
-        TextView v = (TextView) LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.my_text_view, parent, false);
-        ...
-        MyViewHolder vh = new MyViewHolder(v);
-        return vh;
-    }
-
-    // Replace the contents of a view (invoked by the layout manager)
-    @Override
-    public void onBindViewHolder(MyViewHolder holder, int position) {
-        // - get element from your dataset at this position
-        // - replace the contents of the view with that element
-        holder.textView.setText(mDataset[position]);
-
-    }
-
-    // Return the size of your dataset (invoked by the layout manager)
-    @Override
-    public int getItemCount() {
-        return mDataset.length;
-    }
-}
-
+//package com.example.gather;
+//
+//import android.view.LayoutInflater;
+//import android.view.ViewGroup;
+//import android.widget.TextView;
+//import com.example.gather.Event;
+//
+//import androidx.recyclerview.widget.RecyclerView;
+//
+//public class EventAdapter extends RecyclerView.Adapter<EventAdapter.MyViewHolder> {
+//    private Event[] mDataset;
+//
+//// Provide a reference to the views for each data item
+//// Complex data items may need more than one view per item, and
+//// you provide access to all the views for a data item in a view holder
+//    public static class MyViewHolder extends RecyclerView.ViewHolder {
+//        // each data item is just a string in this case
+//        public TextView textView;
+//        public MyViewHolder(TextView v) {
+//            super(v);
+//            textView = v;
+//        }
+//    }
+//
+//    // Provide a suitable constructor (depends on the kind of dataset)
+//    public EventAdapter(Event[] myDataset) {
+//        mDataset = myDataset;
+//    }
+//
+//    // Create new views (invoked by the layout manager)
+//    @Override
+//    public EventAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent,
+//                                                        int viewType) {
+//        // create a new view
+//        TextView v = (TextView) LayoutInflater.from(parent.getContext())
+//                .inflate(R.layout.my_text_view, parent, false);
+//        ...
+//        MyViewHolder vh = new MyViewHolder(v);
+//        return vh;
+//    }
+//
+//    // Replace the contents of a view (invoked by the layout manager)
+//    @Override
+//    public void onBindViewHolder(MyViewHolder holder, int position) {
+//        // - get element from your dataset at this position
+//        // - replace the contents of the view with that element
+//        holder.textView.setText(mDataset[position]);
+//
+//    }
+//
+//    // Return the size of your dataset (invoked by the layout manager)
+//    @Override
+//    public int getItemCount() {
+//        return mDataset.length;
+//    }
+//}
+//
--- app/src/main/java/com/example/gather/FindEventsActivity.java
+++ app/src/main/java/com/example/gather/FindEventsActivity.java
@@ -1,18 +1,35 @@
 package com.example.gather;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.gather.Event;
 import com.example.gather.User;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.example.gather.EventAdapter;
+//import com.example.gather.EventAdapter;
 
-public class FindEventsActivity extends AppCompatActivity {
+public class FindEventsActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    Button buttonCreateEvent;
+    FrameLayout testFrame;
+
+    private CreateEventFragment createEventFragment;
+
+    private FirebaseAuth mAuth;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("Users");
 
     private RecyclerView recyclerView;
     private RecyclerView.Adapter mAdapter;
@@ -22,6 +39,13 @@ public class FindEventsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_events);
+
+        testFrame = findViewById(R.id.test_frame);
+
+        buttonCreateEvent = findViewById(R.id.buttonCreateEvent);
+        buttonCreateEvent.setOnClickListener(this);
+
+        createEventFragment = new CreateEventFragment();
 
         recyclerView = (RecyclerView) findViewById(R.id.findEventsRecyclerView);
 
@@ -34,7 +58,20 @@ public class FindEventsActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
 
         // specify an adapter
-        mAdapter = new EventAdapter(myDataset); // TODO Write function to pull in list of events from database
+//        mAdapter = new EventAdapter(myDataset); // TODO Write function to pull in list of events from database
         recyclerView.setAdapter(mAdapter);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
+        setFragment(createEventFragment);
+    }
+
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.test_frame, fragment); //TODO replace with UI to hold the fragment interface
+        fragmentTransaction.commit();
     }
 }
